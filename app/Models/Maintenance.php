69791921b83fb32f811b6a9f61b81872ee37257b
AbarraSaad <153920304+abarrasaad@users.noreply.github.com>
2026-01-26 22:00:12 +0100

    Added fillable, casts and resource function to maintenance model

--- app/Models/Maintenance.php
+++ app/Models/Maintenance.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Maintenance extends Model
 {
     //
-    public $timestamps = false;
+    protected $fillable = ['resource_id', 'start_time', 'end_time', 'reason'];
+    protected $casts = [
+        'start_time' => 'datetime',
+        'end_time' => 'datetime',
+    ];
+    public function resource()
+    {
+        return $this->belongsTo(Resource::class);
+    }
 }
