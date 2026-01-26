bcbabd6a8e2c9d7e1225be2639c746ef84a5a65f
AbarraSaad <153920304+abarrasaad@users.noreply.github.com>
2026-01-26 21:43:44 +0100

    Added casts, fillables, and manager function to the resource model

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Resource extends Model
 {
     //
-    public $timestamps = false;
+    protected $casts = [
+        'is_active' => 'boolean',
+        'specifications' => 'array'
+    ];
+    protected $fillable = ['name', 'category_id', 'manager_id', 'specifications', 'is_active'];
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+    public function manager() {
+        return $this->belongsTo(User::class, 'manager_id');
+    }
 }
