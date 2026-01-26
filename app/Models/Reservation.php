6679b5f517dd9e97e25e56dece6611c6afa08332
AbarraSaad <153920304+abarrasaad@users.noreply.github.com>
2026-01-26 21:56:36 +0100

    Added fillables, casts and resources, user function to Reservation model

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     //
-    public $timestamps = false;
+    protected $fillable = ['user_id', 'resource_id', 'start_time', 'end_time', 'status', 'justification', 'admin_note'];
+    protected $casts = [
+        'start_time' => 'datetime',
+        'end_time' => 'datetime'
+    ];
+    public function resource() {
+        return $this->belongsTo(Resource::class);
+    }
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
