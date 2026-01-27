7398726688d5fb57f930720ef6f580e308c84960
AbarraSaad <153920304+abarrasaad@users.noreply.github.com>
2026-01-27 03:31:39 +0100

    Updated te UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use app\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,5 +15,28 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         //
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'admin@datacenter.com',
+            'password' => Hash::make('password'), // Password is 'password'
+            'role_id' => 1, // Points to 'admin' role
+            'is_active' => true,
+        ]);
+
+        User::create([
+            'name' => 'Technician Bob',
+            'email' => 'tech@datacenter.com',
+            'password' => Hash::make('password'),
+            'role_id' => 2, // Points to 'manager' role
+            'is_active' => true,
+        ]);
+
+        User::create([
+            'name' => 'Professor Charlie',
+            'email' => 'user@datacenter.com',
+            'password' => Hash::make('password'),
+            'role_id' => 3, // Points to 'user' role
+            'is_active' => true,
+        ]);
     }
 }
