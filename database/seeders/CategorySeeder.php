3d78eb6e128530b34630fdf18facae8e582df36c
AbarraSaad <153920304+abarrasaad@users.noreply.github.com>
2026-01-27 03:40:11 +0100

    Fixed an issue with capitalization

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use app\Models\Category;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
